40249283f1a6b5c0222d21bbcf2f8bf2635dd0b0
daniel.chou <daniel0422@gmail.com>
2015-06-21 23:03:03 +0800

    git init my test project.

---
+++ mysite/trips/views.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from django.http import HttpResponse
+
+
+def hello_world(request):
+    output = """
+        <!DOCTYPE html>
+        <html>
+            <head>
+            </head>
+            <body>
+                Hello World! <em style="color:LightSeaGreen;">{current_time}</em>
+            </body>
+        </html>
+    """.format(current_time=datetime.now())
+
+    return HttpResponse(output)
